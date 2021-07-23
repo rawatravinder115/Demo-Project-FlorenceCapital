5152ce5326c869e0484002e2d47a4fe8b7570b22
rawatravinder115 <rawatravinder115@gmail.com>
2021-07-23 15:54:23 +0530

    add lombok dependency

--- demo/src/main/java/DemoProjectFlorenceCapital/demo/Entity/snacks.java
+++ demo/src/main/java/DemoProjectFlorenceCapital/demo/Entity/snacks.java
@@ -1,10 +1,30 @@
 package DemoProjectFlorenceCapital.demo.Entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.lang.reflect.GenericArrayType;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@Document(name=)
 public class snacks {
 
+    @Id
     private int id ;
     private  String snacks;
     private String shop;
-    private
-    
+    @Field("imageurl")
+    private String image;
+
 }
