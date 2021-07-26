62966be2941ffb08a3fb788f8cc6b3ec0c93773d
rawatravinder115 <rawatravinder115@gmail.com>
2021-07-26 22:58:53 +0530

    remove unrelivent dependency

--- demo/src/main/java/DemoProjectFlorenceCapital/demo/Config/MongoConfig.java
+++ demo/src/main/java/DemoProjectFlorenceCapital/demo/Config/MongoConfig.java
@@ -1,10 +1,7 @@
 package DemoProjectFlorenceCapital.demo.Config;
 
 import  com.mongodb.*;
-import com.mongodb.client.MongoClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.MongoDbFactory;
